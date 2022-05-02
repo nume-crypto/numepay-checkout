7281aa81dd2a7c501d66d1f8cf10f910f413f4af
Vikram <vikram710v@gmail.com>
2022-05-03 02:45:27 +0530

    Fix: Change checkout and backend url

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const axios = require('axios');
 const checkoutWithNume = async (payload) => {
     try {
+        let backendAPI = "http://44.201.174.127:3000/api/create-transaction"
         const response = await axios({
             method: 'post',
-            url: 'http://localhost:3000/api/create-transaction',
+            url: backendAPI,
             data: payload,
         });
 
         const top = window.top.outerHeight / 2 + window.top.screenY - 750 / 2;
         const left = window.top.outerWidth / 2 + window.top.screenX - 750 / 2;
+        let numeCheckoutUrl = "https://master--fascinating-parfait-941cda.netlify.app"
         let newWindow = window.open(
-            `http://localhost:5000/${response.data.message.OrderId}`,
+            `${numeCheckoutUrl}/${response.data.message.OrderId}`,
             'nume',
             `height=750,width=750,top=${top}, left=${left}`
         );
         if (window.focus) {
             newWindow.focus();
         }
+        console.log(newWindow)
         return new Promise((accept, reject) => {
             const handler = (e) => {
-                if (e.origin !== 'http://localhost:5001') {
+                if (e.origin === numeCheckoutUrl) {
                     try {
                         accept(e.data);
                     } catch (error) {
