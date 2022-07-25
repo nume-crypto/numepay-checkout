62842e7815ab061be64d3ee77427f49be130f695
Vikram <vikram710v@gmail.com>
2022-07-25 14:05:57 +0530

    Fix: safari opener and popup in new tab

--- index.js
+++ index.js
@@ -15,74 +15,80 @@ const authorize = async (clientId, clientSecret) => {
 	const response = await (await fetch(url, options)).json();
 	return response.message.AccessToken;
 };
-const checkoutWithNume = async (payload) => {
-	try {
-		let accessToken = payload.accessToken;
-		delete payload.accessToken;
-		let url = `${API_URL}merchant/create-transaction`;
-		let options = {
-			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${accessToken}`,
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(payload),
-		};
 
-		let data = await (await fetch(url, options)).json();
-		const top = window.top.outerHeight / 2 + window.top.screenY - 750 / 2;
-		const left = window.top.outerWidth / 2 + window.top.screenX - 750 / 2;
-
-		let newWindow = window.open(
-			`${CHECKOUT_URL}${data.message.OrderId}`,
-			'nume',
-			`height=750,width=750,top=${top}, left=${left}`
-		);
-		if (window.focus) {
-			newWindow.focus();
-		}
-		return new Promise(async (accept, reject) => {
-			const handler = async (e) => {
-				if (CHECKOUT_URL.includes(e.origin)) {
-					try {
-						let url = `${API_URL}merchant/order-details/${data.message.OrderId}`;
-						let options = {
-							method: 'GET',
-							headers: {
-								Authorization: `Bearer ${accessToken}`,
-								Accept: 'application/json',
-								'Content-Type': 'application/json',
-							},
-						};
-
-						let response = await (await fetch(url, options)).json();
-						let res = {
-							orderId: response.message.Order.OrderId,
-							orderStatus: response.message.Order.Status,
-							amount: response.message.Order.Amount,
-							currency: response.message.Order.CurrencyId,
-							payee: response.message.Order.UserId,
-						};
-						if (res.orderStatus !== 'APPROVED') {
-							reject(res);
-						} else {
-							accept(res);
-						}
-					} catch (error) {
-						reject(error);
-					}
-					window.removeEventListener('message', handler);
-				}
-			};
-			window.addEventListener('message', handler);
-		});
-	} catch (error) {
-		console.error(error);
-		return new Promise((accept, reject) => {
-			reject(error);
-		});
+const checkoutWithNume = (payload) => {
+	let accessToken = payload.accessToken;
+	delete payload.accessToken;
+	let url = `${API_URL}merchant/create-transaction`;
+	let options = {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(payload),
+	};
+	let newWindow = window.open();
+	if (window.focus) {
+		newWindow.focus();
 	}
+	return fetch(url, options)
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			if (!newWindow) {
+				return new Promise((accept, reject) => {
+					reject('Window closed');
+				});
+			}
+			newWindow.location.assign(`${CHECKOUT_URL}${data.message.OrderId}`);
+			if (window.focus) {
+				newWindow.focus();
+			}
+			return new Promise(async (accept, reject) => {
+				const handler = async (e) => {
+					if (CHECKOUT_URL.includes(e.origin)) {
+						try {
+							let url = `${API_URL}merchant/order-details/${data.message.OrderId}`;
+							let options = {
+								method: 'GET',
+								headers: {
+									Authorization: `Bearer ${accessToken}`,
+									Accept: 'application/json',
+									'Content-Type': 'application/json',
+								},
+							};
+
+							let response = await (await fetch(url, options)).json();
+							let res = {
+								orderId: response.message.Order.OrderId,
+								orderStatus: response.message.Order.Status,
+								amount: response.message.Order.Amount,
+								currency: response.message.Order.CurrencyId,
+								payee: response.message.Order.UserId,
+							};
+							if (res.orderStatus !== 'APPROVED') {
+								reject(res);
+							} else {
+								accept(res);
+							}
+						} catch (error) {
+							reject(error);
+						}
+						window.removeEventListener('message', handler);
+					}
+				};
+				window.addEventListener('message', handler);
+			});
+		})
+		.catch((error) => {
+			console.error(error);
+			return new Promise((accept, reject) => {
+				reject(error);
+			});
+		});
 };
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
